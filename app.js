6b2646fe660b76d1b212244e4d8129052e44c9da
Ben Bryant <brbryant2639@gmail.com>
2020-01-26 19:50:48 -0600

    finished js code for console

---
+++ app.js
@@ -0,0 +1,8 @@
+let friends = ["Wil", "Sim", "Mola", "Fela", "Denna"];
+
+for(i=0; i<friends.length; i++){
+    for(x=99; x>0; x--){
+        console.log(x + " lines of code in the file, " + x + " lines of code in the file, " 
+        + friends[i] + " strikes one out, clears it all out, " + (x-1) + " lines of code in the file.")
+    };
+};
